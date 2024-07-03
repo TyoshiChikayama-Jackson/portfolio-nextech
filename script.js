836b0d303a61a1436c93800093f5334409fd84a0
TyoshiChikayama-Jackson <tyoshijackson@gmail.com>
2024-07-02 23:09:02 -0500

    updates

--- script.js
+++ script.js
@@ -1,57 +1,124 @@
 let header = document.querySelector(".header"); 
-let aboutMe = document.querySelector(".aboutMe");
-let achievement=document.querySelector(".achievement");
+let aboutMeSection = document.querySelector(".aboutMeSection");
+let sectionachievements = document.querySelector(".sectionachievements");
 let future = document.querySelector(".future");
 let project = document.querySelector(".project"); 
+let contact = document.querySelector(".contact");
+
 // This is the pages that need to be shown or hidden //
-let aboutMeSection = document.querySelector(".aboutMeSection");
-let home = document.querySelector(".home")
+
+let home = document.querySelector(".home");
+let aboutMe = document.querySelector(".aboutMe");
+let skills = document.querySelector(".skills");
+let futureaspirations = document.querySelector(".futureaspirations");
+let myprojects = document.querySelector(".myprojects"); 
+let contacting = document.querySelector(".contacting");
 
 // This is the timeline in achievements and skills //
-let groupstexttimelineoneone = document.querySelector(".groupstexttimelineoneone")
-let groupstexttimelinetwotwo = document.querySelector(" .groupstecttimelinetwotwo"); 
-let groupstexttimelinethreethree = document.querySelector(" .groupstexttimelinethreethree"); 
-let groupstexttimelinefourfour = document.querySelector(" .groupstexttimelinefourfour"); 
-
-
-let groupstexttimelineone = document.querySelector(".groupstexttimelineone"); 
-let groupstexttimelinetwo = document.querySelector(".groupstexttimelinetwo"); 
-let awardtext = document.querySelector(".awardtext")
-let awardtexttwo = document.querySelector(".awardtexttwo")
-
-//document.getElementById('first').addEventListener("click", function(){
-// }); 
-
-// document.getElementById('second').addEventListener("click", function(){
-//     header.style.display= "none"; 
-//      
-// }); 
-// aboutMeSection.addEventListener("click", function(){
-//     header.style.display ="none"; 
-//     aboutMeSection.style.display ="block"; 
-    
-// });
-
+// nav bar!!!!!
 home.addEventListener("click", function(){
     header.style.display = "block"; 
     aboutMeSection.style.display = "none"; 
+    sectionachievements.style.display = "none"; 
+    future.style.display = "none"; 
+    project.style.display = "none"; 
+    contact.style.display = "none";
+    copyrightprojects.style.display ="none"; 
 }); 
-
-
 aboutMe.addEventListener("click", function(){
     header.style.display= "none";
     aboutMeSection.style.display ="block";
+    sectionachievements.style.display = "none";
+    future.style.display = "none";  
+    project.style.display = "none"; 
+    contact.style.display = "none";
+    copyrightprojects.style.display ="none"; 
 }); 
-
-
-
-
-groupstexttimelinetwotwo.addEventListener("click", function(){
-    groupstexttimelineone.style.display ="none"; 
-    awardtext.style.disply ="none"; 
-    groupstexttimelinetwo.style.display = "inline-block";
-    awardtexttwo.style.display = "inline-block"; 
+skills.addEventListener("click", function() {
+    header.style.display= "none";
+    aboutMeSection.style.display ="none";
+    sectionachievements.style.display= "block";
+    future.style.display = "none"; 
+    project.style.display = "none";
+    contact.style.display = "none";
+    copyrightprojects.style.display ="none"; 
 });
+futureaspirations.addEventListener("click", function() {
+    header.style.display= "none";
+    aboutMeSection.style.display ="none";
+    sectionachievements.style.display= "none";
+    future.style.display ="block"; 
+    project.style.display = "none";
+    contact.style.display = "none";
+    copyrightprojects.style.display ="none"; 
+});
+myprojects.addEventListener("click", function() {
+    header.style.display= "none";
+    aboutMeSection.style.display ="none";
+    sectionachievements.style.display= "none";
+    future.style.display ="none"; 
+    project.style.display = "block";
+    contact.style.display = "none";
+    copyright.style.display = "none"; 
+});
+contacting.addEventListener("click", function() {
+    header.style.display= "none";
+    aboutMeSection.style.display ="none";
+    sectionachievements.style.display= "none";
+    future.style.display ="none"; 
+    project.style.display = "none";
+    contact.style.display = "block";
+    copyrightprojects.style.display ="none";
 
+});
+// This is all the extra fun I have done in my achievements
+let youthOfTheDayOne = document.querySelector(".youthOfTheDayOne");
+let varsityBasketballManagerOne = document.querySelector(".varsityBasketballManagerOne"); 
+let nationalHonorsSocietyOne = document.querySelector(".nationalHonorsSocietyOne"); 
+let businessClubPresidentOne = document.querySelector(".businessClubPresidentOne"); 
 
-let groupstext = document.querySelector(".groupstext");
+let youthOfTheDayImg = document.querySelector(".youthOfTheDayImg");
+let varsityBasketballManagerImg = document.querySelector(".varsityBasketballManagerImg");
+let nationalHonorsSocietyImg = document.querySelector(".nationalHonorsSocietyImg");
+let businessClubPresidentImg = document.querySelector(".businessClubPresidentImg");
+
+youthOfTheDayImg.addEventListener("click", function(){
+    youthOfTheDayOne.style.display = "block"
+    varsityBasketballManagerOne.style.display = "none"
+    nationalHonorsSocietyOne.style.display = "none"
+    businessClubPresidentOne.style.display = "none"
+    youthOfTheDayOne.setAttribute('id','animated');
+    varsityBasketballManagerOne.setAttribute('id','');
+    nationalHonorsSocietyOne.setAttribute('id','');
+    businessClubPresidentOne.setAttribute('id','');
+}); 
+varsityBasketballManagerImg.addEventListener("click", function(){
+    youthOfTheDayOne.style.display = "none"
+    varsityBasketballManagerOne.style.display = "block"
+    nationalHonorsSocietyOne.style.display = "none"
+    businessClubPresidentOne.style.display = "none"
+    youthOfTheDayOne.setAttribute('id','');
+    varsityBasketballManagerOne.setAttribute('id','animated');
+    nationalHonorsSocietyOne.setAttribute('id','');
+    businessClubPresidentOne.setAttribute('id','');
+}); 
+nationalHonorsSocietyImg.addEventListener("click", function(){
+    youthOfTheDayOne.style.display = "none"
+    varsityBasketballManagerOne.style.display = "none"
+    nationalHonorsSocietyOne.style.display = "block"
+    businessClubPresidentOne.style.display = "none"
+    youthOfTheDayOne.setAttribute('id','');
+    varsityBasketballManagerOne.setAttribute('id','');
+    nationalHonorsSocietyOne.setAttribute('id','animated');
+    businessClubPresidentOne.setAttribute('id','');
+}); 
+businessClubPresidentImg.addEventListener("click", function(){
+    youthOfTheDayOne.style.display = "none"
+    varsityBasketballManagerOne.style.display = "none"
+    nationalHonorsSocietyOne.style.display = "none"
+    businessClubPresidentOne.style.display = "block"
+    youthOfTheDayOne.setAttribute('id','');
+    varsityBasketballManagerOne.setAttribute('id','');
+    nationalHonorsSocietyOne.setAttribute('id','');
+    businessClubPresidentOne.setAttribute('id','animated');
+}); 
